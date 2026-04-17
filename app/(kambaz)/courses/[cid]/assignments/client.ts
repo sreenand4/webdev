6013842b27485947851855228b1f47be9842b0dd
sreenandm <148009692+sreenandm@users.noreply.github.com>
2026-04-17 05:57:31 -0400

    a6 env vars

--- app/(kambaz)/courses/[cid]/assignments/client.ts
+++ app/(kambaz)/courses/[cid]/assignments/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER || "";
 const COURSES_API = `${HTTP_SERVER}/api/courses`;
 const ASSIGNMENTS_API = `${HTTP_SERVER}/api/assignments`;
 
